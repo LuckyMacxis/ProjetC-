67a8f6814f9c303b1be2eacce858c99bb171e268
LuckyMacxis <aimeric13@live.fr>
2018-04-13 14:25:05 +0200

    ok

--- CListeMatrice.cpp
+++ CListeMatrice.cpp
@@ -26,15 +26,47 @@ void CListeMatrice::LISajouter(string  sChemin)
 	ifstream monFlux(sChemin);  //Ouverture du fichier en lecture
 	if (monFlux)
 	{
-		string ligne;
-		getline(monFlux, ligne);
-		ligne.substr(12, 10);
+		string stype;
+		unsigned int iNbLignes, iNbColonnes;
+		string sligne;
 
-		while (getline(monFlux, ligne))
+		getline(monFlux, sligne);
+		stype=sligne.substr(14,10);
+
+		getline(monFlux, sligne);
+		sligne=sligne.substr(9, 10);
+		iNbLignes = stoi(sligne, nullptr,5);
+
+		getline(monFlux, sligne);
+		sligne = sligne.substr(11, 5);
+		iNbColonnes = stoi(sligne, nullptr, 5);
+
+		getline(monFlux, sligne);
+
+		double **Matrice = new double*[iNbLignes];
+		for (int i = 0; i < iNbLignes; i++)
+			Matrice[i] = new double[iNbColonnes];
+		int indicateur=0;
+		while (indicateur!=iNbLignes)
 		{
-			cout << ligne << "\n";
+			getline(monFlux, sligne);
+			unsigned int i; int j = 0;
+			for (i = 0; i <= iNbColonnes; i=i+2)
+			{
+				Matrice[indicateur][j] = stod(sligne);
+				sligne = sligne.substr(2, iNbColonnes*2);
+				j++;
+			}
+			Matrice[indicateur][j] = stod(sligne);
+			indicateur++;
 		}
-		cout << "okligne";
+		uiLIStaille++;
+		//MatriceList = new CMatrice<double>[uiLIStaille];
+		CMatrice<double> Mat(Matrice,iNbLignes,iNbColonnes);
+		Mat.MATafficherMatrice();
+		/*for (int i = 0; i < iNbLignes; i++)
+			delete[] Matrice[i];
+		delete[] Matrice;*/
 	}
 	else
 	{
